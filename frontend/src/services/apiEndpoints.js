971f6520f9fb378da0d3d75f43a1e2336a0c47f5
shivamkumar125156 <shivam125156@gmail.com>
2025-04-20 16:38:33 +0530

    url changed

--- frontend/src/services/apiEndpoints.js
+++ frontend/src/services/apiEndpoints.js
@@ -1,10 +1,21 @@
 
-const BASE_URL=import.meta.env.VITE_BACKEND_BASE_URL
+// const BASE_URL=import.meta.env.VITE_BACKEND_BASE_URL
 
-export const SIGNUP_API=BASE_URL+"/api/v1/user/signup";
-export const LOGIN_API=BASE_URL+"/api/v1/user/login";
-export const UPDATE_PROFILE_API=BASE_URL+"/api/v1/user/update";
-export const FIND_USERS_API=BASE_URL+"/api/v1/user/bulk";
+// export const SIGNUP_API=BASE_URL+"/api/v1/user/signup";
+// export const LOGIN_API=BASE_URL+"/api/v1/user/login";
+// export const UPDATE_PROFILE_API=BASE_URL+"/api/v1/user/update";
+// export const FIND_USERS_API=BASE_URL+"/api/v1/user/bulk";
 
-export const USER_BALANCE_API=BASE_URL+"/api/v1/account/balance"
-export const TRANSFER_MONEY_API= BASE_URL+"/api/v1/account/transfer"
+// export const USER_BALANCE_API=BASE_URL+"/api/v1/account/balance"
+// export const TRANSFER_MONEY_API= BASE_URL+"/api/v1/account/transfer"
+
+
+
+
+export const SIGNUP_API="https://paymentapp-hacl.onrender.com/api/v1/user/signup";
+export const LOGIN_API="https://paymentapp-hacl.onrender.com/api/v1/user/login";
+export const UPDATE_PROFILE_API="https://paymentapp-hacl.onrender.com/api/v1/user/update";
+export const FIND_USERS_API="https://paymentapp-hacl.onrender.com/api/v1/user/bulk";
+
+export const USER_BALANCE_API="https://paymentapp-hacl.onrender.com/api/v1/account/balance"
+export const TRANSFER_MONEY_API= "https://paymentapp-hacl.onrender.com/api/v1/account/transfer"
